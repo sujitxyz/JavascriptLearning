bb0334ef95bf4eaf6031d1478f03cb53c6763d7a
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-06-05 10:04:03 +0000

    Comparision of data is learned

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,26 @@
+/*------------- Comparision-----------------*/
+  // > :Greater than 
+  // < :Lesser than 
+  // == :equal to 
+  //=! : not equal to 
+  // >= greater than or equal to
+  // <= lesser than or equal to 
+
+// comparison should be done in same datatypes 
+
+console.log(2>1);
+
+console.log(null > 0); // false
+console.log(null == 0); //false
+console.log(null >=0); //true
+
+console.log(undefined >0); //false
+console.log(undefined == 0); //false
+console.log(undefined >=0); //false
+
+// avoid this kind of comparison
+
+// ===: use for strict checking 
+console.log("1" === 1); //false
+
+
