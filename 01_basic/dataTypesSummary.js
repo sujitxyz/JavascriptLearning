4241d15286ec2024bfa572d8bd32c15ee2681226
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-06-11 16:49:24 +0000

    datatype summary

---
+++ 01_basic/dataTypesSummary.js
@@ -0,0 +1,21 @@
+/* 
+  There are two types of datatypes:
+  1) Primitive 
+      :String
+      :Number 
+      :Boolean 
+      :Null
+      :Undefined
+      :Symbol
+      :BigInt 
+*/
+/*
+  2)Non-Primitive 
+       :Function
+       :Object
+       :Array
+*/
+//js is dynamically typed language 
+
+let score = 100;
+//it doesnt need to defined a typed 
