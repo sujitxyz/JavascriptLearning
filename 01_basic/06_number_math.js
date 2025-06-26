3c08d76aae69d71f053c646933f851222f092938
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-06-26 13:43:36 +0000

    concept of math is learned

--- 01_basic/06_number_math.js
+++ 01_basic/06_number_math.js
@@ -19,5 +19,36 @@ console.log(payment.toLocaleString('en-In'));//it convert number into number sys
 
 
 
+/*********Math****************/
+
+
+
+console.log(Math); //it is object 
+
+console.log(Math.abs(-4));//its provide absolute value 
+
+console.log(Math.round(3.5)); //it round of the value
+
+console.log(Math.ceil(5.2)); //it round of the value but upper value//6 
+
+console.log(Math.floor(5.2)); //it round of the value but lower value //5
+
+console.log(Math.min(5, 4, 9, 1)); //it help to find minimum value  //1
+
+console.log(Math.max(5, 4, 9, 1)); //it help to find maximum value //9
+
+console.log(Math.random()); //provide random value between 0-1 
+
+console.log((Math.random()*10) + 1); // provide value 1-10
+
+console.log(Math.floor(Math.random()*10) + 1);// give value 1 to 10 
+
+//Most important formula 
+
+const min = 100
+const max = 200
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
 
 
