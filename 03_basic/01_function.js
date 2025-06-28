cf53916f9cc0349c15d594c57f6f337a0756de55
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-06-28 18:37:11 +0000

    function and parameter is learned

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -10,3 +10,23 @@ function testFunction(){
 
 //now executing function 
 testFunction()
+                     //parameter
+//function mathOperator(number1, number2) {
+  //console.log( number1 + number2);
+//}
+function mathOperator(number1, number2) {
+ //let result= number1 + number2
+// return result
+return number1 + number2
+}
+
+//mathOperator(1, 2) //arguments
+
+let result = mathOperator(1,2)
+console.log(`Result: ${result}`);
+
+function isSleeping(userName){
+    return `${userName} is sleeping`
+}
+ console.log(isSleeping("Sujit"));
+ 
