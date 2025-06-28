5de53ba52e09c05304c952c09669512cc98f8d0f
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-06-28 14:44:04 +0000

    learned about how to execute function

---
+++ 03_basic/01_function.js
@@ -0,0 +1,12 @@
+//this is how function is written 
+
+function testFunction(){
+    console.log("S");
+    console.log("u");
+    console.log("j");
+    console.log("i");
+    console.log("t");
+}
+
+//now executing function 
+testFunction()
