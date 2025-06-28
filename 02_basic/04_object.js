3130745e8157238b07337ccc0a7927c869a75d27
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-06-28 11:12:07 +0000

    more basics of object is learned

---
+++ 02_basic/04_object.js
@@ -0,0 +1,72 @@
+//Singleton Object
+
+let instaUser = new Object()
+
+
+//literal object is
+//let FaceBookUser = {}
+//console.log(FaceBookUser);
+
+instaUser.id = "Sujitxyz"
+instaUser.name = "Sujit"
+instaUser.isloggedin = true
+
+
+
+
+
+//object inside object (Nested object)
+
+const user1 = {
+    idNumber: 1,
+    fullName: {
+        firstName: "Sujit",
+        lastName: "Bhusal"
+    },
+    address: "Kathmandu"
+}
+console.log(user1.fullName.firstName);
+
+//combining two or more object 
+
+const obj1 = {1: "a", 2: "B"}
+const obj2 ={3: "c", 4: "D"}
+
+//const obj3 = { obj1, obj2} wrong approach 
+
+
+//const obj3 = Object.assign({},obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+//how value come from database
+const user2 =[ {
+    idNumber: 1,
+    address: "Kathmandu"
+},
+ {
+    idNumber: 1,
+    address: "Kathmandu"
+},
+ {
+    idNumber: 1,
+    address: "Kathmandu"
+},
+ {
+    idNumber: 1,
+    address: "Kathmandu"
+}]
+
+//to access the value 
+console.log(user2[1]);
+
+console.log(instaUser)
+console.log(Object.keys(instaUser));//it shows the key of object 
+console.log(Object.values(instaUser));//it show the value of object
+
+
+ console.log(instaUser.hasOwnProperty('Name'));//it show proproties exist or not 
+
+ 
+                   
