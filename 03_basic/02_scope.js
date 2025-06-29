c8a7fd16df9d2af98349b100f61d323a3bf47118
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-06-29 11:04:31 +0000

    learned about nested scope and  some level of hoisting

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -19,3 +19,47 @@ console.log(a); //it will show a= 300 cause of global scope
 //console.log(b);
 //console.log(c);
 
+ //nested scope 
+
+ function one(){
+    const username = "Sujit"  // scope one
+
+    function two(){  // scope two
+
+        const address = "Kathmandu"
+        console.log(username); //for scope 2 username is global scope 
+    }
+    //console.log(address); //it only available in scope 2 so address is not declared in scope 1
+    
+    two()
+ }
+ one()
+ 
+
+ if (true){ //scope1
+     const userName = "Sujit"
+     if(userName === "Sujit"){ //Scope2
+     const address = "Kathmandu"
+     console.log(`${userName} ${address}`); // username is global scope of scope 2
+     }
+     //console.log(address); it doesnt contain in scope1
+ }
+ //console.log(userName); it contain inside scope1 and not contain in global scope
+
+ 
+  //Concept of Hoisting
+
+ console.log(User(5))
+  function User(Id){
+      return Id       //we can write this way function is not stored in variables 
+  }
+ // User(5)
+
+
+//console.log(someData(2)); Cannot access 'someData' before initialization
+//because function is stored in varaiable
+  const someData = function(Id2){
+    return Id2
+  }
+
+ // someData(2)
