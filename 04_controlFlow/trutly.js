655c45c4a6611987aa3d9871890e1b1cd3647e8b
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-07-02 18:38:11 +0000

    learned about Nullish coalescing operator (??)

--- 04_controlFlow/trutly.js
+++ 04_controlFlow/trutly.js
@@ -27,3 +27,24 @@ if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
     
 }
+
+// Nullish coalescing operator (??) null or undefined
+
+let val1;
+//val1 = 5 ?? 10
+//val1 = 10 ?? 5
+//it gives first value 
+
+//val1 = null ?? 10
+//val1 = 10 ?? null
+//it avoid null which gives error
+
+//val1 = undefined ?? 20
+//val1 =  20 ?? undefined 
+
+val1 = null ?? 20 ?? undefined
+
+
+//this operator avoid null and undefined
+
+console.log(val1);
