86602dd81babbb0ae18b45b8de5248d75b7265bd
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-07-02 18:26:02 +0000

    learned about switch case

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,22 @@
+let myNumber = "hundred"
+
+switch (myNumber) {
+    case "one":
+        console.log("one")
+        break;
+    case "ten":
+        console.log("ten")
+        break;        
+    case "hundred":
+        console.log("hundred")
+        break;        
+    case "thousand":
+        console.log("thousand")
+        break;          
+
+    default:
+        console.log("no value found");
+        break;
+}
+
+//break is used to stop over compilation
