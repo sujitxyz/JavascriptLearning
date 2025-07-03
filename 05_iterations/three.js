31e5270ad3c2954baec396cbe7411d33cb1ed796
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-07-03 12:30:49 +0000

    learned about forof loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,20 @@
+//for of loop 
+
+// ["", "", "" ]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+for (const num of arr) {
+   // console.log(num)
+    
+}
+
+const greetings = "Hello World!"
+
+for (const greets of greetings) {
+    if (greets === " ") {
+        continue
+    }
+    //console.log(greets);
+    
+}
