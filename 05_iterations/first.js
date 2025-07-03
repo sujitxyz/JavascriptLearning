0e4207c1febfdcad32396f3a75cfd3a662e07e3f
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-07-03 09:23:28 +0000

    learned about break and continue in for loop

--- 05_iterations/first.js
+++ 05_iterations/first.js
@@ -33,6 +33,46 @@ let placesInNepal = ["Kathmandu", "Lalitpur", "Chitwan"]
 
 for (let i = 0; i < placesInNepal.length; i++) {
   const element = placesInNepal[i];
-  console.log(element);
+  //console.log(element);
   
 }
+
+//break and continue 
+
+ for (let j = 1; j < 10; j++) {
+  if (j === 5) {
+   // console.log(`5 is best number`);
+   // break
+  }
+   const element = j;
+   //console.log(element);
+}
+//above code will print upto 5 is best number 
+/*
+1
+2
+3
+4
+5 is best number
+*/
+
+for (let j = 1; j < 10; j++) {
+  if (j === 5) {
+   console.log(`5 is best number`);
+  continue
+  }
+   const element = j;
+   console.log(element);
+}
+// it will continue printing output after printing 5 is best number
+/*
+1
+2
+3
+4
+5 is best number
+6
+7
+8
+9
+*/
