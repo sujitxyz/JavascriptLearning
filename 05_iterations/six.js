754d8ea4d9914936af7aef1f53b238e71e721abf
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-07-03 14:25:29 +0000

    learn about filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,31 @@
+// //const coding = ["javascript",
+//      "C++",
+//      "ruby",
+//      "python",
+//      "C"
+//]
+// const values = coding.forEach((items) => {
+//     // console.log(items)
+//     return items
+// })
+
+// console.log(values);
+
+
+// for each doesnt return value
+
+
+//filter
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNums.filter( (num) => num > 5)
+console.log(newNums);
+   
+
+// ()=> {return} return must written to executed code
+//filter is used when condition is needed 
+
+  
+
+
