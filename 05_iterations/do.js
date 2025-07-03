f37291d16b4b58d3f9cb66f96566a9554c878c05
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-07-03 09:38:25 +0000

    learned about while loop

---
+++ 05_iterations/do.js
@@ -0,0 +1,14 @@
+//while loop 
+
+let i = 1
+while (i <= 10) {
+    //console.log(i);
+    i += 2
+}
+//while loop in array
+let placesInNepal = ["Kathmandu", "Lalitpur", "Chitwan"]
+let j = 0
+while(j < placesInNepal.length){
+   console.log(placesInNepal[j]);
+   j++
+}
