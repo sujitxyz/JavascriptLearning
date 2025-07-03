8cf7ac712fc053d6c142c2f08822fb3bb1d7518b
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-07-03 14:56:56 +0000

    chaining of map and filter is learned

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,5 +1,11 @@
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-//const newNums = myNums.map((nums) => nums + 10)
+// const newNums = myNums.map((num) => num + 10)
+// console.log(newNums);
 
+
+//chaining 
+const newNums = myNums.map((num) => num * 10)
+                      .map((num) => num + 10)
+                      .filter((num) => num > 50)
 console.log(newNums);
