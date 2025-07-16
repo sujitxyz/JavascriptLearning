84dfd1463d80c0977cadd6da02d45659c1e1564c
Sujit Bhusal <159654760+sujitxyz@users.noreply.github.com>
2025-07-16 15:11:55 +0000

    learned about how classes work behind the scence

--- 10_classes/classPractice.js
+++ 10_classes/classPractice.js
@@ -15,4 +15,16 @@ const user1 = new user("Sujit", "email@email.com", "1234")
 console.log(user1.encryptPassword());
 
 
+//behind the scences
 
+function user2(username, email, password) {
+         this.username = username
+         this.email = email
+         this.password = password
+}
+user2.prototype.encryptPassword = function () {
+   return `${this.password}xyz`
+}
+
+const user2 = new user2("Sujit", "email@email.com", "1234")
+console.log(user2.encryptPassword());
